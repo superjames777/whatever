a9bd3c3066e875a76d502e12275e450e437ed560
superjames777 <superjames777.777@gmail.com>
2026-08-26 22:10:54 -0400

    Finished inject command

--- gullible.js
+++ gullible.js
@@ -1,3 +1,8 @@
-okButton = document.getElementById("okbutton")
+const okButton = document.getElementById("okbutton");
+const toDel = document.getElementById("delafter");
+const after = document.getElementById("afterclick")
 
-
+okButton.addEventListener("click", () => {
+    toDel.remove();
+    after.textContent = "gullible";
+});
